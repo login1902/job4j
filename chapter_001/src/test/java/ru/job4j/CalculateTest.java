742b4864e5c5a6af6722a3a2e5033acdb191e931
login1902 <sergey190287@gmail.com>
2018-04-11 21:25:01 +0400

    Text

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -7,22 +7,22 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Sergey Loginov
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Sergey Loginov
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/ @Test
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Sergey Loginov";
-    String expect = "Echo, echo, echo : Sergey Loginov"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+    /**
+     * Test echo.
+     */ @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Sergey Loginov";
+        String expect = "Echo, echo, echo : Sergey Loginov";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
 }
