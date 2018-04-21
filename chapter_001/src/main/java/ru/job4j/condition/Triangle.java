1b6ab9c2b5f0e0dd522936d667fd39917fab2539
login1902 <sergey190287@gmail.com>
2018-04-21 20:56:52 +0400

    3.3. Вычисление площади треугольника [#9461]

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -10,3 +10,29 @@ public class Triangle {
         this.b = b;
         this.c = c;
     }
+
+    public double period(double ab, double ac, double bc) {
+        return (ab + ac + bc) / 2;
+    }
+
+    public double area() {
+        double rsl= -1;
+        double ab = this.a.distanceTo(this.b);
+        double ac = this.a.distanceTo(this.c);
+        double bc = this.b.distanceTo(this.c);
+        double p = this.period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            rsl = Math.sqrt(p * (p - ab) * (p - ac) * (p - bc));
+        }
+            return rsl;
+
+    }
+    private boolean exist(double ab, double ac, double bc) {
+        return (ab + ac > bc && ab + bc > ac && ac + bc >ab) ? true : false;
+        }
+}
+
+
+
+
+
--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -2,7 +2,15 @@ package ru.job4j.max;
 
 public class Max {
 
-    public int max(int first, int second) {
+    public int max(int first, int second)
+    {
         return first >= second ? first : second;
             }
+
+    public int max2(int first, int second, int third){
+        int temp = this.max(first, second);
+        return temp = this.max(temp, third);
+
+    }
+
 }
